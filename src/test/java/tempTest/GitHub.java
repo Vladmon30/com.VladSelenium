54bce21520c041d4bdcb4b54f01d2e184dcfed8e
Vlad <vladmonqa35@gmail.com>
2020-01-30 15:27:32 -0600

    adding new GitHubTest Class

---
+++ src/test/java/tempTest/GitHub.java
@@ -0,0 +1,12 @@
+package tempTest;
+
+import org.testng.annotations.Test;
+
+public class GitHub {
+    @Test
+    public void GitHubTest(){
+         int commits =10;
+        System.out.println(commits);
+    }
+
+}
--- src/test/java/tempTest/TempTest.java
+++ src/test/java/tempTest/TempTest.java
@@ -3,6 +3,9 @@ package tempTest;
 import org.testng.annotations.Test;
 
 @Test
+
+
+
 public class TempTest {
     public void printTest(){
         System.out.println("Print some changes");
