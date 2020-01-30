a92a851fbae6884a15626eccd0dda1c69ae03da0
Vlad <vladmonqa35@gmail.com>
2020-01-30 12:58:59 -0600

    second commit. add new class

---
+++ src/test/java/tempTest/TempTest.java
@@ -0,0 +1,10 @@
+package tempTest;
+
+import org.testng.annotations.Test;
+
+@Test
+public class TempTest {
+    public void printTest(){
+        System.out.println("Print some changes");
+    }
+}
